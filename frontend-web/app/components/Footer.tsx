7e3f2786af3d56c7fd02f42769cc71c45dbafec6
arvaridwan554 <arvaridwan554@gmail.com>
2026-08-12 21:51:09 +0700

    Create Footer.tsx

---
+++ frontend-web/app/components/Footer.tsx
@@ -0,0 +1,28 @@
+export default function Footer() {
+  return (
+    <footer className="bg-black text-white py-10 mt-10 text-center">
+      <h2 className="font-bold text-lg mb-4">
+        TrackIQ
+      </h2>
+
+      <p className="text-sm mb-2">
+        stp.its.ac.id
+      </p>
+
+      <p className="text-sm mb-5">
+        +6285852157263
+      </p>
+
+      <div className="flex justify-center gap-5 text-sm mb-6">
+        <span>Instagram</span>
+        <span>Tiktok</span>
+        <span>YouTube</span>
+        <span>Facebook</span>
+      </div>
+
+      <div className="border-t border-gray-700 pt-4 text-xs">
+        © 2026 Sains Tecno Park. All rights reserved.
+      </div>
+    </footer>
+  );
+}
